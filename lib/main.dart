be22c9ab1b8547882af32ae165c52d6b5b942b16
AnnaPaltusova <anna.l.andronova@gmail.com>
2022-03-11 22:05:57 +0500

    демо темы

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'utils/global_theme.dart';
+import 'utils/strings.dart';
+import 'theme_demo.dart';
+
+
+void main() {
+  runApp(const ThemesDemoApp());
+}
