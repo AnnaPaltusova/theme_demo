09d02949bdafcaac06dd2253e0ea09e78898194b
AnnaPaltusova <anna.l.andronova@gmail.com>
2022-03-14 20:39:20 +0300

    работа со списками ListView

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'utils/global_theme.dart';
 import 'utils/strings.dart';
 import 'theme_demo.dart';
-
+import 'ListView.dart';
 
 void main() {
-  runApp(const ThemesDemoApp());
+  runApp(const ListViewScreen());
 }
