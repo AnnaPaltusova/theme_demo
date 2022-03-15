52531daed4a974435c8ac4cb7aaf53efb58810cd
AnnaPaltusova <anna.l.andronova@gmail.com>
2022-03-15 16:35:59 +0500

    Хранение данных

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'utils/global_theme.dart';
 import 'utils/strings.dart';
 import 'theme_demo.dart';
 import 'ListView.dart';
+import 'shared_pref.dart';
+import  'files_demo_screen.dart';
 
 void main() {
-  runApp(const ListViewScreen());
+  runApp(const FilesDemoScreen());
 }
