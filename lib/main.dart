6a71225705bbce8b62d07e6dd48bfa2124c09d27
AnnaPaltusova <anna.l.andronova@gmail.com>
2022-04-04 23:44:49 +0500

    https

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,10 @@ import 'theme_demo.dart';
 import 'ListView.dart';
 import 'shared_pref.dart';
 import  'files_demo_screen.dart';
-
+import 'networking_screen.dart';
+import 'switches_demo.dart';
 
 
 void main() {
-  runApp(const FilesDemoScreen());
+  runApp(const NetworkingScreen());
 }
